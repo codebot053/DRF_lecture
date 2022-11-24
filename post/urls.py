3094673335b997a1f828e91a8c008a734748e9c8
codebot053 <codebot053@gmail.com>
2022-11-24 21:55:03 +0900

    init: 프로젝트 초기설정

---
+++ post/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post',views.PostViewSet) # 2개의 URL을 만들어준다.
+                                          # 만들어진 두개의 URL패턴은 router.urls에 있다.
+
+urlpatterns = [
+    path('',include(router.urls)),
+
+]
---
+++ post/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+from .serializers import Postserializer
+from .models import Post
+# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
