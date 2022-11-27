c92f2eb7482e8c88835cd74c932667a7e91945c3
codebot053 <codebot053@gmail.com>
2022-11-27 16:41:46 +0900

    refactor: post.views 리펙토링

--- post/urls.py
+++ post/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('post',views.PostViewSet) # 2개의 URL을 만들어준다.
+router.register('post', views.PostViewSet) # 2개의 URL을 만들어준다.
                                           # 만들어진 두개의 URL패턴은 router.urls에 있다.
 
 urlpatterns = [
     path('',include(router.urls)),
-    path("list/",views.PostListApiView.as_view()),
+    path("list/", views.PostListApiView.as_view()),
 ]
