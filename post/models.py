7afb391a29385fb06d3b51e1f9b3e6b7cf232ff1
codebot053 <codebot053@gmail.com>
2022-11-28 18:39:02 +0900

    modified: Post 테이블에 author 필드 추가

--- post/models.py
+++ post/models.py
@@ -1,9 +1,10 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
