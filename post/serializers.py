ea802b52d34ba3efd29e4a43f992018fada04885
codebot053 <codebot053@gmail.com>
2022-11-28 18:41:49 +0900

    feat: AuthorSerializer 추가 post/serialziers.py 에서 Post 모델의 fk 인 author을 중첩된 serialzier인 AuthorSerialize를 생성하여 기존 Postserializer에서 author 필드값으로 사용할 수 있도록 구현

--- post/serializers.py
+++ post/serializers.py
@@ -1,7 +1,33 @@
-from rest_framework .serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from .models import Post
 
-class Postserializer(ModelSerializer):
+
+class AuthorSerializer(serializers.ModelSerializer):
+    # Post 모델의 fk author을 중첩된 serializer를 통해서 구현
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'username',
+            'email'
+        ]
+
+
+
+class Postserializer(serializers.ModelSerializer):
+    # username = serializers.ReadOnlyField(source='author.username')
+    # post 모델에서 fk 로 참조하는 user의 username을 읽을수 있다. CASE 1
+    author = AuthorSerializer()
+    # serializer 중첩
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'pk',
+            'author',
+            'title',
+            'content',
+            'created_at',
+            'updated_at',
+
+        ]
+
