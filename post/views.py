9d8dd385adae304bbef33e01eef7c9700746828f
codebot053 <codebot053@gmail.com>
2022-11-30 21:38:25 +0900

    feat: post_list 뷰 처리 추가

--- post/views.py
+++ post/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import Postserializer
 from .models import Post
 
@@ -19,3 +21,17 @@ class PostViewSet(ModelViewSet):
 class PostListApiView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = Postserializer
+
+# 하나의 작업만을 구현하고자 할때 @api_view를 쓰면 편리합니다.
+@api_view(['GET', 'POST'])
+def post_list(request):
+    
+    if request.method == 'GET':
+        serializer = Postserializer(Post.objects.all(), many=True)
+        return Response(serializer.data, status=200)
+    else:
+        serializer = Postserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
