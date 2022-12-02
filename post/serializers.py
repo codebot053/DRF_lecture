c369c14671f1c00a91f18d33daa36713eda11e8c
codebot053 <codebot053@gmail.com>
2022-12-02 18:58:51 +0900

    modified: PostSerializer 필드값 변경 author, author_eamil 을 read only로 추가

--- post/serializers.py
+++ post/serializers.py
@@ -3,23 +3,25 @@ from rest_framework import serializers
 from .models import Post
 
 
-class AuthorSerializer(serializers.ModelSerializer):
-    # Post 모델의 fk author을 중첩된 serializer를 통해서 구현
-    class Meta:
-        model = get_user_model()
-        fields = ["username", "email"]
+# class AuthorSerializer(serializers.ModelSerializer):
+#     # Post 모델의 fk author을 중첩된 serializer를 통해서 구현
+    
+#     class Meta:
+#         model = get_user_model()
+#         fields = ["username", "email"]
 
 
-class Postserializer(serializers.ModelSerializer):
-    # username = serializers.ReadOnlyField(source='author.username')
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    author_email = serializers.ReadOnlyField(source='author.email')
     # post 모델에서 fk 로 참조하는 user의 username을 읽을수 있다. CASE 1
-    author = AuthorSerializer()
+    # author = AuthorSerializer()
     # serializer 중첩
     class Meta:
         model = Post
         fields = [
-            "pk",
             "author",
+            "author_email",
             "title",
             "content",
             "created_at",
