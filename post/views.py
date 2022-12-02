d5974355b202c99ec263f480d8ccc6b90e07f790
codebot053 <codebot053@gmail.com>
2022-12-02 19:05:41 +0900

    feat: PostViewSet perform_create 추가

--- post/views.py
+++ post/views.py
@@ -1,37 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import Postserializer
+from .serializers import PostSerializer
 from .models import Post
 
 
 class PostViewSet(ModelViewSet):
+    '''
+    postlist, postdetail 의 기능 CRRUD를 담당합니다.
+    list, create => /post/
+    detail(retrieve), update, Destroy => /post/<int:pk>/
+    '''
     queryset = Post.objects.all()
-    serializer_class = Postserializer
+    serializer_class = PostSerializer
 
     def dispatch(self, request, *args, **kwargs):
         print("request.body :", request.body)  # 클라이언트의 인코딩에 따라 다른결과를 확인할 수 있다.
         print("request.POST :", request.POST)
         return super().dispatch(request, *args, **kwargs)
-
-    # 해당 요청이 올때마다 실행
-
-
-class PostListApiView(ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = Postserializer
-
-# 하나의 작업만을 구현하고자 할때 @api_view를 쓰면 편리합니다.
-@api_view(['GET', 'POST'])
-def post_list(request):
     
-    if request.method == 'GET':
-        serializer = Postserializer(Post.objects.all(), many=True)
-        return Response(serializer.data, status=200)
-    else:
-        serializer = Postserializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+    def perform_create(self, serializer):
+        '''
+        해당 유저정보를 form, json을 통하지 않고 request 통해 저장
+        '''
+        serializer.save(author=self.request.user)
