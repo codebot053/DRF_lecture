7bcc869f5bb84d7c0bc4e45fd7db81f57f61e4c7
codebot053 <codebot053@gmail.com>
2022-12-05 16:11:48 +0900

    feat: PostDetailAPIView url 매핑

--- post/urls.py
+++ post/urls.py
@@ -1,12 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 
-router = DefaultRouter()
-router.register("post", views.PostViewSet)  # 2개의 URL을 만들어준다.
-# 만들어진 두개의 URL패턴은 router.urls에 있다.
-
 urlpatterns = [
-    path("", include(router.urls)),
-    # path("list/", views.PostListApiView.as_view()),
+    path("detail/<int:pk>/", views.PostDetailAPIView.as_view(), name="detail"),
 ]
