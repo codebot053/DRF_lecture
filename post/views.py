a0f13d04cdbda239cd3e26768292f0b0bf17093c
codebot053 <codebot053@gmail.com>
2022-12-05 15:54:11 +0900

    feat: PostViewSet에 public, set_public 메서드 추가 공개처리된 글의 리스트만 모아보는 public 해당 글 객체의 is_public 값을 변경하는 set_public

--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from .models import Post
@@ -15,6 +15,31 @@ class PostViewSet(ModelViewSet):
     '''
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        '''
+        is_public 필드값이 True인 글들만 리스트 합니다.
+        '''
+        qs = self.queryset.filter(is_public=True)
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['PATCH'])
+    def set_public(self, request, pk):
+        '''
+        is_public 필드값을 True 로 변경 합니다.
+        '''
+        instance = self.get_object()
+        public_status = instance.is_public
+        if public_status:
+            public_status = False
+        else:
+            public_status = True
+        instance.is_public = public_status
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     def dispatch(self, request, *args, **kwargs):
         print("request.body :", request.body)  # 클라이언트의 인코딩에 따라 다른결과를 확인할 수 있다.
