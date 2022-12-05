bd13d5570b4b24ebcfa7117d82968e20fb8dad33
codebot053 <codebot053@gmail.com>
2022-12-05 15:49:30 +0900

    modified: Post 모델에 is_public 필드 추가

--- post/models.py
+++ post/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     content = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
