2f0ee5b224b9fc28c95d0342ba31124283fca547
codebot053 <codebot053@gmail.com>
2022-12-06 23:22:21 +0900

    modified: PostViewSet dispatch 메소드 제거 필요없는 메소드 삭제

--- post/views.py
+++ post/views.py
@@ -41,11 +41,6 @@ class PostViewSet(ModelViewSet):
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
-    def dispatch(self, request, *args, **kwargs):
-        print("request.body :", request.body)  # 클라이언트의 인코딩에 따라 다른결과를 확인할 수 있다.
-        print("request.POST :", request.POST)
-        return super().dispatch(request, *args, **kwargs)
-    
     def perform_create(self, serializer):
         '''
         해당 유저정보를 form, json을 통하지 않고 request 통해 저장
